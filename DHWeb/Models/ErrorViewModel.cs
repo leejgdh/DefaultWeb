95a92bc99b27a14f63122cd1b722cd90f6bcf23a
leejgdh <leejgdh@ecremmoce.onmicrosoft.com>
2021-10-10 13:52:58 +0900

    init web

---
+++ DHWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DHWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
